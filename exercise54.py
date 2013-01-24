d07bcc9bb87508e9ecc32028d0a4aa39a851ce91
chrisvans <chrisvanschyndel@gmail.com>
2013-01-24 02:49:59 -0500

    Exercise54 and koch_curve

--- exercise54.py
+++ exercise54.py
@@ -1,2 +1,34 @@
 # This is where Exercise 5.4 goes
-# Name:
+# Name: Christopher Van Schyndel
+
+
+def is_triangle(a, b, c):
+	if a > b + c or b > a + c or c > a + b:
+		print "No"
+		return False
+	else:
+		print "Yes"
+		return True
+
+def stickinput():
+	print "This program will compare three side lengths of a triangle and respond with"
+	print "whether or not such a triangle is possible."
+	a = raw_input("Enter the first triangle side length: ")
+	print "1) " + str(a)
+	b = raw_input("Now the second side length, please: ")
+	print "1) " + str(a) + "  2) " + str(b)
+	c = raw_input("The third and last side length, please: ")
+	print "1) " + str(a) + "  2) " + str(b) + "  3) " + str(c)
+	if a.isdigit() == False or b.isdigit() == False or c.isdigit() == False:
+		print "Please input integers only!"
+	else:
+		a = int(a)
+		b = int(b)
+		c = int(c)
+		print "Is the triangle possible with the lengths given?"
+		is_triangle(a, b, c)
+
+
+stickinput()
+
+
